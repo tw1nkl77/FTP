14f2b5b2cf54e2e526459450039d5bc8a0ca7c12
Rustam <danchoo14@bk.ru>
2022-04-06 01:49:20 +0300

    1

--- BullsAndCows/src/components/const/types.js
+++
@@ -1,9 +0,0 @@
-export default {
-    templatesBulls: {
-        getTemplate(item) {
-            return `<div class="result-attempt">
-                <p>Выбранная вами комбинация: <b>${item.choice}</b>. Быки: <b>${item.bulls}</b>, коровы: <b>${item.cows}</b>. Осталось: <b>${item.left} попыток!</b></p>
-            </div>`;
-        }
-    }
-};
