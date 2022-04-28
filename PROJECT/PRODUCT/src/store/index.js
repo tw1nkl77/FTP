61ad45c49659a11ee2e5b454b66e40458c14b849
Rustam Schultz <danchoo14@bk.ru>
2022-04-28 13:25:27 +0300

    1

--- PROJECT/PRODUCT/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'vuex';
-import Catalog from './modules/catalog';
-import Cart from './modules/cart';
-import DescriptionProduct from './modules/descriptionProduct';
-
-export default createStore({
-    modules: { Catalog, Cart, DescriptionProduct, ShippingMethods },
-});
