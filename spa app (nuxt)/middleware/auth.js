7f3e195a9aa1cb7de5320d1a650e5bb3eaddf6fe
tw1nkl77 <103740195+tw1nkl77@users.noreply.github.com>
2022-05-30 23:54:25 +0300

    Delete spa app (nuxt) directory

--- spa app (nuxt)/middleware/auth.js
+++
@@ -1,6 +0,0 @@
-export default function ({ store, redirect }) {
-    const { token } = store.getters;
-    if (!token) {
-        return redirect("/authorization")
-    };
-};
--- spa app (nuxt)/pages/index.vue
+++
@@ -1,5 +0,0 @@
-<script>
-export default {
-  middleware: ["auth"],
-};
-</script>
